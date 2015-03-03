6e0e6ac4af4ad31a60802a3d5ecb1bf1d8a03f7e
Robert Young <yong.yang@agrant.cn>
2015-03-03 15:03:10 +0000

    C++

--- digest.cpp
+++ digest.cpp
@@ -1,31 +1,17 @@
 /*
 
-http://download.microsoft.com/download/d/2/4/d242c3fb-da5a-4542-ad66-f9661d0a8d19/vcredist_x64.exe
-http://slproweb.com/download/Win64OpenSSL-1_0_1g.exe
-http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/4.8.2/threads-posix/seh/x86_64-4.8.2-release-posix-seh-rt_v3-rev3.7z/download
-
-g++ -Wall -Wextra digest.cpp libeay32.dll -lz -o digest.exe
+g++ -std=c++11 -Wall -Wextra -O3 digest.cpp -lcrypto -lz -lboost_program_options -lboost_filesystem -lboost_system -lboost_date_time -o digest
 
 */
 
-#include <Windows.h>
-#include <malloc.h>
-#include <stdio.h>
-#include <dirent.h>
 #include <sys/stat.h>
-#include <assert.h>
-#include <string.h>
-#include <limits.h>
-#include <time.h>
 #include <fcntl.h>
 #include <openssl/evp.h>
 #include <zlib.h>
-#include <map>
-#include <string>
-#include <algorithm>
-
-#undef PATH_MAX
-#define PATH_MAX 1024
+#include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/format.hpp>
 
 struct md
  {const EVP_MD *md;
@@ -53,115 +39,105 @@ void final(std::map<std::string,md>::iterator x)
  } 
 
 void print(std::map<std::string,md>::iterator x)
- {fprintf(stdout,"%s:",EVP_MD_name(x->second.md));
+ {std::cout<<EVP_MD_name(x->second.md)<<':';
   for(unsigned int i=0;i<x->second.size;i++)
-   {fprintf(stdout,"%02x",x->second.r[i]);
+   {std::cout<<boost::format("%02x")%((static_cast<short>(x->second.r[i])&0xFF));
    }
-  fprintf(stdout,"\t");
+  std::cout<<"\t";
  } 
 
-int traversal(const wchar_t *d,FILE *f,bool r)
- {_WDIR *dp;
-  _wdirent *ep;
-  assert(dp=_wopendir(d));
-  for(;(ep=_wreaddir(dp));)
-   {
-    wchar_t fname[PATH_MAX+1];
-    //wchar_t *fname=(wchar_t*)malloc((PATH_MAX+1)*sizeof(wchar_t));
-    //wchar_t *fname=(wchar_t*)_alloca((PATH_MAX+1)*sizeof(wchar_t));
-    //wchar_t *fname=new wchar_t[PATH_MAX+1];
-    wcscpy(fname,d);
-    wcscat(fname,L"\\");
-    wcscat(fname,ep->d_name);
-    struct _stat64 s;
-    assert(_wstat64(fname,&s)==0);
-    if(S_ISDIR(s.st_mode))
-     {if(!(((ep->d_name)[0]=='.'&&(ep->d_name)[1]==0)||((ep->d_name)[0]=='.'&&(ep->d_name)[1]=='.'&&(ep->d_name)[2]==0)))
-       {traversal(fname,f,r); 
-       }
-     }
-    else
-     {
-      if(!S_ISREG(s.st_mode))
-       {assert(fname);}
-      else
-       {off64_t i;
-        char fname_UTF8[(PATH_MAX+1)*4];
-        //char *fname_UTF8=(char*)_alloca((PATH_MAX+1)*sizeof(char)*4);
-        //fprintf(stdout,"%d\t",wcslen(fname));
-        //fwprintf(stdout,L"%s\t",fname);
-        //memset (fname_UTF8,0,(PATH_MAX+1)*4);
-        assert(0<WideCharToMultiByte( CP_UTF8, 0, fname, -1, fname_UTF8, (PATH_MAX+1)*4, NULL, NULL )); /* TAB CAN NOT BE A VALID FILENAME */
-        fprintf(stdout,"%s\t",fname_UTF8);
-        uLong crc=crc32(0L,Z_NULL,0);
-        std::map<std::string,md> x;
-        x["MD5"]=md();
-        x["SHA1"]=md();
-        x["SHA256"]=md();
-        x["SHA512"]=md();
-        x["RIPEMD160"]=md();
-        x["whirlpool"]=md();
-        for(std::map<std::string,md>::iterator it=x.begin();it!=x.end();it++) init(it);
-        const size_t block_size=128;
-        unsigned char buf[block_size];
-        if(r)
-         {int fi;
-          off64_t fsize;
-          assert(-1!=(fi=_wopen(fname,O_RDONLY|_O_BINARY)));
-          for(fsize=0;(i=read(fi,buf,block_size))==block_size;fsize+=block_size)
-           {crc = crc32(crc, buf, block_size);
-            for(std::map<std::string,md>::iterator it=x.begin();it!=x.end();it++) update(it,buf,block_size);
-           }
-          close(fi);
-          assert(fsize+i==s.st_size);
-          crc=crc32(crc, buf, i);
-          for(std::map<std::string,md>::iterator it=x.begin();it!=x.end();it++) update(it,buf,i);
-          for(std::map<std::string,md>::iterator it=x.begin();it!=x.end();it++) final(it);
-         }
-        strftime((char*)fname,block_size,"%Y%m%d%H%M%S",localtime(&(s.st_mtime)));
-        fprintf(stdout,"%s\t",(char*)fname);
-        strftime((char*)fname,block_size,"%Y%m%d%H%M%S",localtime(&(s.st_ctime)));
-        fprintf(stdout,"%s\t",(char*)fname);
-        fprintf(stdout,"%I64d\t",s.st_size/*,s.st_ctime_usec*/);
-        fprintf(stdout,"CRC32:%08lX\t",crc);
-        for(std::map<std::string,md>::iterator it=x.begin();it!=x.end();it++) print(it);
+int main(int argc, char *argv[])
+ {boost::program_options::options_description desc;
+  boost::filesystem::path p;
+  std::string o;
+  bool c;
+  std::string s;
+  bool r;
+  desc.add_options()
+   ("help,h", "1.1.1.1")
+   ("path,p",boost::program_options::value<boost::filesystem::path>(&p)->default_value("."),"Where to Traversal")
+   ("out,o",boost::program_options::value<std::string>(&o)->default_value("-"),"Output")
+   ("content,c", boost::program_options::value<bool>(&c)->default_value(false),"Calculate file digest")
+   //("digests,s", boost::program_options::value<std::string>(&s)->default_value("crc32,md5,sha1,sha256,sha512,ripemd160,whirlpool"),"Digests to be selected")
+   ("regular_file_only,r", boost::program_options::value<bool>(&r)->default_value(true),"Exception of non-regular_file");
 
-        fprintf(stdout,"\n");
-        fflush(stdout);
-        //fprintf(stdout," %d,%d,%d\n",block_size,EVP_MAX_MD_SIZE,size);
-       }
-     }
-    //free(fname);
-    //delete []fname;
+  boost::program_options::variables_map vm;
+  boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
+  boost::program_options::notify(vm);
+  if(vm.count("help"))
+   {std::cerr<<desc<<"\n";
+    return(1);
    }
-  _wclosedir(dp);
-  return(0);
- }
-
-int main(int argc,char *argv[])
- {wchar_t d[PATH_MAX+1];
-  if(argc<2)
-   {wcscpy(d,L"");}
   else
-   {MultiByteToWideChar(CP_ACP,MB_ERR_INVALID_CHARS,argv[1],-1,d,PATH_MAX);
-    if(argc>=3)
-     {if(argv[2][0]=='-' && argv[2][1]==0 )
-       {
+   {p=boost::filesystem::canonical(p);
+    if("-"!=o)
+     {assert(freopen(o.c_str(),"w",stdout));
+     }
+    OpenSSL_add_all_digests();
+    for (boost::filesystem::recursive_directory_iterator i(p),e; i!=e; i++)
+     {if(boost::filesystem::is_directory(i->path())){}
+      else if(boost::filesystem::is_regular_file(i->path()))
+       {struct stat64 s;
+        assert(0==stat64(i->path().c_str(),&s));
+        assert(S_ISREG(s.st_mode));
+        std::cout<<i->path()<<"\t";
+        assert(i->path().native().size()<=i->path().native().find_first_of('"'));
+        std::cout<<boost::posix_time::to_iso_string(boost::posix_time::from_time_t(s.st_mtime))<<"\t";
+        std::cout<<boost::posix_time::to_iso_string(boost::posix_time::from_time_t(s.st_ctime))<<"\t";
+        std::cout<<s.st_size<<"\t"<<std::flush;
+        if(c)
+         {off64_t readed;
+          uLong crc=crc32(0L,Z_NULL,0);
+          std::map<std::string,md> x;
+          x["MD5"]=md();
+          x["SHA1"]=md();
+          x["SHA256"]=md();
+          x["SHA512"]=md();
+          x["RIPEMD160"]=md();
+          x["whirlpool"]=md();
+          for(std::map<std::string,md>::iterator m=x.begin();m!=x.end();m++) init(m);
+          const size_t block_size=128;
+          unsigned char buf[block_size];
+          int f;
+          off64_t fsize;
+          assert(-1!=(f=open64(i->path().c_str(),O_RDONLY)));
+          for(fsize=0;(readed=read(f,buf,block_size))==block_size;fsize+=block_size)
+           {crc = crc32(crc, buf, block_size);
+            for(std::map<std::string,md>::iterator m=x.begin();m!=x.end();m++) update(m,buf,block_size);
+           }
+          close(f);
+          assert(fsize+readed==s.st_size);
+          crc=crc32(crc, buf, readed);
+          for(std::map<std::string,md>::iterator m=x.begin();m!=x.end();m++) update(m,buf,readed);
+          for(std::map<std::string,md>::iterator m=x.begin();m!=x.end();m++) final(m);
+          std::cout<<"CRC32:"<<boost::format("%08lX")%crc<<"\t";
+          for(std::map<std::string,md>::iterator m=x.begin();m!=x.end();m++) print(m);
+         }
+        std::cout<<"\n";
        }
       else
-       {assert(freopen(argv[2],"w",stdout));
+       {if(r)
+         {std::map<boost::filesystem::file_type,std::string> s=
+           {{boost::filesystem::status_unknown,"status_unknown"},
+            {boost::filesystem::file_not_found,"file_not_found"},
+            {boost::filesystem::regular_file,"regular_file"},
+            {boost::filesystem::directory_file,"directory_file"},
+            {boost::filesystem::symlink_file,"symlink_file"},
+            {boost::filesystem::block_file,"block_file"},
+            {boost::filesystem::character_file,"character_file"},
+            {boost::filesystem::fifo_file,"fifo_file"},
+            {boost::filesystem::socket_file,"socket_file"},
+            {boost::filesystem::reparse_file,"reparse_file"},
+            {boost::filesystem::type_unknown,"type_unknown"},
+            {boost::filesystem::_detail_directory_symlink,"_detail_directory_symlink"}
+           };
+          std::cerr<<"NOT a regular file: "<<i->path()<<" : file_type="<<s[boost::filesystem::status(i->path()).type()]<<std::endl;
+          assert(boost::filesystem::is_regular_file(i->path()));
+         }
        }
-      bool r;
-      if(argc==4)
-       {if(stricmp(argv[3],"0")==0)
-         {r=false;}
-        else
-         {r=true;}
-       }
-      OpenSSL_add_all_digests();
-      traversal(d,stdout,r);
-      fclose(stdout);
      }
+    fclose(stdout);
+    return(0);
    }
-   return(0);
  }
+

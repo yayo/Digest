be866b95464a924e4244dac0270b3468d65e501e
Robert Young <yong.yang@agrant.cn>
2015-03-12 09:21:05 +0000

    Allowing single File digest calculation

--- digest.cpp
+++ digest.cpp
@@ -42,8 +42,6 @@ g++ -std=c++11 -Wall -Wextra -O3 digest.cpp -o digest libboost_program_options-m
 #include <boost/program_options.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/format.hpp>
-//#include <zlib.h>
-//#include <isc/crc64.h> // /usr/lib64/libisc.so.142.4.0
 
 struct md
  {const EVP_MD *md;
@@ -78,6 +76,55 @@ void print(std::map<std::string,md>::iterator x)
   std::cout<<"\t";
  } 
 
+void digest(const boost::filesystem::path &p,const bool &c)
+ {struct STATE_S s;
+  assert(0==STATE_F(p.c_str(),&s));
+  assert(S_ISREG(s.st_mode));
+  std::cout<<p<<"\t";
+  assert(p.native().size()<p.native().find_first_of('"'));
+  std::cout<<boost::posix_time::to_iso_string(boost::posix_time::from_time_t(s.st_mtime))<<"\t";
+  std::cout<<boost::posix_time::to_iso_string(boost::posix_time::from_time_t(s.st_ctime))<<"\t";
+  std::cout<<s.st_size<<"\t"<<std::flush;
+  if(c)
+   {off64_t readed;
+    boost::crc_32_type crc32_ieee;
+    boost::crc_optimal<64, 0x42F0E1EBA9EA3693ULL,0xFFFFFFFFFFFFFFFFULL,0xFFFFFFFFFFFFFFFFULL,false,false> crc64_ecma_182 ;
+    std::map<std::string,md> x;
+    x["MD4"]=md();
+    x["MD5"]=md();
+    x["MDC2"]=md();
+    x["SHA"]=md();
+    x["SHA1"]=md();
+    x["SHA224"]=md();
+    x["SHA256"]=md();
+    x["SHA384"]=md();
+    x["SHA512"]=md();
+    x["RIPEMD160"]=md();
+    x["whirlpool"]=md();
+    for(std::map<std::string,md>::iterator m=x.begin();m!=x.end();m++) init(m);
+    const size_t block_size=128;
+    unsigned char buf[block_size];
+    int f;
+    off64_t fsize;
+    assert(-1!=(f=OPEN(p.c_str(),O_RDONLY|O_BINARY)));
+    for(fsize=0;(readed=read(f,buf,block_size))==block_size;fsize+=block_size)
+     {crc32_ieee.process_bytes(buf,block_size);
+      crc64_ecma_182.process_bytes(buf,block_size);
+      for(std::map<std::string,md>::iterator m=x.begin();m!=x.end();m++) update(m,buf,block_size);
+     }
+    close(f);
+    assert(fsize+readed==s.st_size);
+    crc32_ieee.process_bytes(buf,readed);
+    crc64_ecma_182.process_bytes(buf,readed);
+    for(std::map<std::string,md>::iterator m=x.begin();m!=x.end();m++) update(m,buf,readed);
+    for(std::map<std::string,md>::iterator m=x.begin();m!=x.end();m++) final(m);
+    std::cout<<"CRC32:"<<boost::format("%08lX")%crc32_ieee.checksum()<<"\t";
+    std::cout<<"CRC64:"<<boost::format("%016lX")%crc64_ecma_182.checksum()<<"\t";
+    for(std::map<std::string,md>::iterator m=x.begin();m!=x.end();m++) print(m);
+   }
+  std::cout<<"\n";
+ }
+
 int main(int argc, char *argv[])
  {boost::program_options::options_description desc;
   boost::filesystem::path p;
@@ -86,7 +133,7 @@ int main(int argc, char *argv[])
   std::string s;
   bool r;
   desc.add_options()
-   ("help,h", "1.1.1.5")
+   ("help,h", "1.1.1.6")
    ("path,p",boost::program_options::value<boost::filesystem::path>(&p)->default_value("."),"Where to Traversal")
    ("out,o",boost::program_options::value<std::string>(&o)->default_value("-"),"Output")
    ("content,c", boost::program_options::value<bool>(&c)->default_value(false),"Calculate file digest")
@@ -106,85 +153,30 @@ int main(int argc, char *argv[])
      {assert(freopen(o.c_str(),"w",stdout));
      }
     OpenSSL_add_all_digests();
-    for (boost::filesystem::recursive_directory_iterator i(p),e; i!=e; i++)
-     {if(boost::filesystem::is_directory(i->path())){}
-      else if(boost::filesystem::is_regular_file(i->path()))
-       {struct STATE_S s;
-        assert(0==STATE_F(i->path().c_str(),&s));
-        assert(S_ISREG(s.st_mode));
-        std::cout<<i->path()<<"\t";
-        assert(i->path().native().size()<i->path().native().find_first_of('"'));
-        std::cout<<boost::posix_time::to_iso_string(boost::posix_time::from_time_t(s.st_mtime))<<"\t";
-        std::cout<<boost::posix_time::to_iso_string(boost::posix_time::from_time_t(s.st_ctime))<<"\t";
-        std::cout<<s.st_size<<"\t"<<std::flush;
-        if(c)
-         {off64_t readed;
-          boost::crc_32_type crc32_ieee;
-          boost::crc_optimal<64, 0x42F0E1EBA9EA3693ULL,0xFFFFFFFFFFFFFFFFULL,0xFFFFFFFFFFFFFFFFULL,false,false> crc64_ecma_182 ;
-          //uLong crc=crc32(0L,Z_NULL,0);
-          //isc_uint64_t crc64_v ;
-          //isc_crc64_init(&crc64_v);
-          std::map<std::string,md> x;
-          x["MD4"]=md();
-          x["MD5"]=md();
-          x["MDC2"]=md();
-          x["SHA"]=md();
-          x["SHA1"]=md();
-          x["SHA224"]=md();
-          x["SHA256"]=md();
-          x["SHA384"]=md();
-          x["SHA512"]=md();
-          x["RIPEMD160"]=md();
-          x["whirlpool"]=md();
-          for(std::map<std::string,md>::iterator m=x.begin();m!=x.end();m++) init(m);
-          const size_t block_size=128;
-          unsigned char buf[block_size];
-          int f;
-          off64_t fsize;
-          assert(-1!=(f=OPEN(i->path().c_str(),O_RDONLY|O_BINARY)));
-          for(fsize=0;(readed=read(f,buf,block_size))==block_size;fsize+=block_size)
-           {crc32_ieee.process_bytes(buf,block_size);
-            crc64_ecma_182.process_bytes(buf,block_size);
-            //crc = crc32(crc, buf, block_size);
-            //isc_crc64_update(&crc64_v,buf,block_size);
-            for(std::map<std::string,md>::iterator m=x.begin();m!=x.end();m++) update(m,buf,block_size);
+    if(boost::filesystem::is_regular_file(p)) digest(p,c);
+    else
+     {for (boost::filesystem::recursive_directory_iterator i(p),e; i!=e; i++)
+       {if(boost::filesystem::is_directory(i->path())){}
+        else if(boost::filesystem::is_regular_file(i->path())) digest(i->path(),c);
+        else
+         {if(r)
+           {std::map<boost::filesystem::file_type,std::string> s=
+             {{boost::filesystem::status_unknown,"status_unknown"},
+              {boost::filesystem::file_not_found,"file_not_found"},
+              {boost::filesystem::regular_file,"regular_file"},
+              {boost::filesystem::directory_file,"directory_file"},
+              {boost::filesystem::symlink_file,"symlink_file"},
+              {boost::filesystem::block_file,"block_file"},
+              {boost::filesystem::character_file,"character_file"},
+              {boost::filesystem::fifo_file,"fifo_file"},
+              {boost::filesystem::socket_file,"socket_file"},
+              {boost::filesystem::reparse_file,"reparse_file"},
+              {boost::filesystem::type_unknown,"type_unknown"},
+              {boost::filesystem::_detail_directory_symlink,"_detail_directory_symlink"}
+             };
+            std::cerr<<"NOT a regular file: "<<i->path()<<" : file_type="<<s[boost::filesystem::status(i->path()).type()]<<std::endl;
+            assert(boost::filesystem::is_regular_file(i->path()));
            }
-          close(f);
-          assert(fsize+readed==s.st_size);
-          crc32_ieee.process_bytes(buf,readed);
-          crc64_ecma_182.process_bytes(buf,readed);
-          //crc=crc32(crc, buf, readed);
-          //assert(crc==crc32_ieee.checksum());
-          //isc_crc64_update(&crc64_v,buf,readed);
-          //isc_crc64_final(&crc64_v);
-          //assert(crc64_v==crc64_ecma_182.checksum());
-          for(std::map<std::string,md>::iterator m=x.begin();m!=x.end();m++) update(m,buf,readed);
-          for(std::map<std::string,md>::iterator m=x.begin();m!=x.end();m++) final(m);
-          std::cout<<"CRC32:"<<boost::format("%08lX")%crc32_ieee.checksum()<<"\t";
-          //std::cout<<"CRC64:"<<boost::format("%016lX")%crc64_v<<"\t";
-          std::cout<<"CRC64:"<<boost::format("%016lX")%crc64_ecma_182.checksum()<<"\t";
-          for(std::map<std::string,md>::iterator m=x.begin();m!=x.end();m++) print(m);
-         }
-        std::cout<<"\n";
-       }
-      else
-       {if(r)
-         {std::map<boost::filesystem::file_type,std::string> s=
-           {{boost::filesystem::status_unknown,"status_unknown"},
-            {boost::filesystem::file_not_found,"file_not_found"},
-            {boost::filesystem::regular_file,"regular_file"},
-            {boost::filesystem::directory_file,"directory_file"},
-            {boost::filesystem::symlink_file,"symlink_file"},
-            {boost::filesystem::block_file,"block_file"},
-            {boost::filesystem::character_file,"character_file"},
-            {boost::filesystem::fifo_file,"fifo_file"},
-            {boost::filesystem::socket_file,"socket_file"},
-            {boost::filesystem::reparse_file,"reparse_file"},
-            {boost::filesystem::type_unknown,"type_unknown"},
-            {boost::filesystem::_detail_directory_symlink,"_detail_directory_symlink"}
-           };
-          std::cerr<<"NOT a regular file: "<<i->path()<<" : file_type="<<s[boost::filesystem::status(i->path()).type()]<<std::endl;
-          assert(boost::filesystem::is_regular_file(i->path()));
          }
        }
      }
